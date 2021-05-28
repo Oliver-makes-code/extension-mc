c31d2728c4b414659dfb815f61f3d5d22cc2737f
Oliver-makes-code <59418903+Oliver-makes-code@users.noreply.github.com>
2021-05-28 14:55:26 -0500

    random changes

--- src/main/java/com/github/olivermakescode/extension/mixin/FarmMixin.java
+++ src/main/java/com/github/olivermakescode/extension/mixin/FarmMixin.java
@@ -22,15 +22,15 @@ public class FarmMixin {
 
     @Redirect(
             at=@At(
-                value="INVOKE",
-                target="Lnet/minecraft/util/math/BlockPos;iterate(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/BlockPos;)Ljava/lang/Iterable;"
+                    value="INVOKE",
+                    target="Lnet/minecraft/util/math/BlockPos;iterate(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/BlockPos;)Ljava/lang/Iterable;"
             ),
             method="isWaterNearby(Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;)Z"
     )
     private static Iterable<BlockPos> iterate(BlockPos start, BlockPos end) {
         int dist = extension.cropWaterRadius.getValue();
-        start.add(4-dist,0,4-dist);
-        end.add(-4+dist,0,-4+dist);
+        start = start.add(4-dist,0,4-dist);
+        end = end.add(dist-4,0,dist-4);
         return BlockPos.iterate(Math.min(start.getX(), end.getX()), Math.min(start.getY(), end.getY()), Math.min(start.getZ(), end.getZ()), Math.max(start.getX(), end.getX()), Math.max(start.getY(), end.getY()), Math.max(start.getZ(), end.getZ()));
     }
 }
